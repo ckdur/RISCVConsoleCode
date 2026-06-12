1ef2d068429b3298d3ed32ac1e44c41bf25a2207
Ckristian Duran <ckdur.iso@gmail.com>
2026-06-12 12:35:53 +0900

    Adding more demos

--- src/test.c
+++ src/test.c
@@ -5,10 +5,18 @@
 #include "dilithium_test.h"
 
 // Run on simulation using: 
+
+// Simulate the comms from FPGA/CHIP
 // make -C sims/vcs run-binary-debug CONFIG=TomohiroConfig BINARY=~/Documents/RISCVConsoleCode/build/out.elf LOADMEM=1 TIMEOUT_CYCLES=0 EXTRA_SIM_PREPROC_DEFINES="+define+UNIT_DELAY=1"
 // make -C sims/verilator run-binary-debug CONFIG=TomohiroConfig BINARY=~/Documents/RISCVConsoleCode/build/out.elf LOADMEM=1 TIMEOUT_CYCLES=0 EXTRA_SIM_PREPROC_DEFINES="+define+UNIT_DELAY=1" USE_FST=1
+// make -C sims/vcs run-binary-debug CONFIG=TomohiroPreChipConfig BINARY=~/Documents/RISCVConsoleCode/build/out.elf LOADMEM=1 TIMEOUT_CYCLES=0 EXTRA_SIM_PREPROC_DEFINES="+define+UNIT_DELAY=1"
+
+
+// Simulate the MCU all-integrated
 // make -C sims/verilator run-binary-debug CONFIG=CryptoMCUConfig BINARY=~/Documents/IKEDA_LAB/RISCVConsoleCode/build/out.elf LOADMEM=1 TIMEOUT_CYCLES=0 EXTRA_SIM_PREPROC_DEFINES="+define+UNIT_DELAY=1" USE_FST=1
 // make -C sims/verilator run-binary-debug CONFIG=CryptoMCUConfig BINARY=~/Documents/IKEDA_LAB/RISCVConsoleCode/build/out.elf LOADMEM=1 TIMEOUT_CYCLES=0 EXTRA_SIM_PREPROC_DEFINES="+define+UNIT_DELAY=1 -I$(pwd)/../tsmc7dig/projects/yuto_dilithium_202604/src" USE_FST=1
+
+// Simulate the TLSerial with FPGA-CHIP comms
 // make -C sims/verilator run-binary-debug CONFIG=TetheredChipCryptoConfig BINARY=~/Documents/IKEDA_LAB/RISCVConsoleCode/build/out.elf LOADMEM=1 TIMEOUT_CYCLES=0 EXTRA_SIM_PREPROC_DEFINES="+define+UNIT_DELAY=1 -I$(pwd)/../tsmc7dig/projects/yuto_dilithium_202604/src" USE_FST=1
 
 // To implement in FPGA for measurements
