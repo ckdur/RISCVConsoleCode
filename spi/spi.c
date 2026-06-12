93e89ab0c2638fbbeb2b63bc2b58850ad633fce1
Ckristian Duran <ckdur.iso@gmail.com>
2026-06-12 21:19:28 +0900

    It moved correctly the PLL

--- spi/spi.c
+++ spi/spi.c
@@ -5,7 +5,7 @@
 
 void spi_init(uint32_t volatile* spi) {
     spi[(SPI_REG_SCKDIV >> 2)] = 5;
-    spi[(SPI_REG_SCKMODE >> 2)] = SPI_SCK_POL;
+    //spi[(SPI_REG_SCKMODE >> 2)] = 0;
     spi[(SPI_REG_CSMODE >> 2)] = 0; // 0 Auto, 2 Hold, 3 Off
 }
 
--- src/test.c
+++ src/test.c
@@ -30,14 +30,14 @@
 uint8_t spi_config[] = {
     // Check signature
     0xBA,
-    // SET_DIV
-    0x4,
-    // FCW
-    0x25,
+    // SET_DIV (FD = F / DIV)
+    6,
+    // FCW (F = REF * (FCW+1))
+    0x8, // about 240MHz can go 
     // KI
-    0x0, 0x0,
+    0x1, 0xFF,
     // KP
-    0x0, 0x0
+    0x0, 0xFF
 };
 const int spi_config_siz = sizeof(spi_config)/sizeof(uint8_t);
 
@@ -66,6 +66,7 @@ static inline int get_gpio(int i) {
 
 void test() {
     while(spi_pll && gpio) {
+        spi_init(spi_pll);
         gpio[(GPIO_INPUT_EN >> 2)] = (1 << 1) | (1 << 3) | (1 << 4) | (1 << 6);
         gpio[(GPIO_OUTPUT_EN >> 2)] = (1 << 0) | (1 << 2) | (1 << 5);
         gpio[(GPIO_PULLUP_EN >> 2)] = 0;
@@ -82,15 +83,23 @@ void test() {
 
         // Push something
         spi_pll[(SPI_REG_CSMODE >> 2)] = SPI_CSMODE_HOLD;
+        // Send a dummy of six
+        spi_pll[(SPI_REG_FMT >> 2)] = SPI_FMT_LEN(5) | SPI_FMT_PROTO(SPI_PROTO_S) | SPI_FMT_ENDIAN(SPI_ENDIAN_MSB)  | SPI_FMT_DIR(SPI_DIR_RX);
+        spi_xfer(spi_pll, 0x0);
+        spi_pll[(SPI_REG_FMT >> 2)] = SPI_FMT_LEN(8) | SPI_FMT_PROTO(SPI_PROTO_S) | SPI_FMT_ENDIAN(SPI_ENDIAN_MSB)  | SPI_FMT_DIR(SPI_DIR_RX);
+        // Send the normal string
         uint8_t back;
         for(int i = 0; i < spi_config_siz; i++) 
             back = spi_xfer(spi_pll, spi_config[i]);
         spi_pll[(SPI_REG_CSMODE >> 2)] = SPI_CSMODE_AUTO;
+        spi_pll[(SPI_REG_CSMODE >> 2)] = SPI_CSMODE_OFF;
+        spi_xfer(spi_pll, 0x0);
+        spi_pll[(SPI_REG_CSMODE >> 2)] = SPI_CSMODE_AUTO;
         
-        if(back != spi_config[0]) {
+        if(back != 0xd0/*0x74*/) {
             // Failed signature
             kprintf("Failed signature in the SPI config (%x)\r\n", (unsigned int) back);
-            continue;
+        //    continue;
         }
         
         // Release the PLL reset
@@ -109,6 +118,7 @@ void test() {
         }
         else {
             kputs("ERROR: PLL did not lock\r\n");
+            continue;
         }
         break;
     }
